e3f1b8792b4c070151368df4bb586aba07344cc2
Sascha Szott <szott@gmx.de>
2018-09-22 19:51:33 +0200

    added 1st assignment 2018

---
+++ ueb1-2018/src/main/java/de/suma/SolrIndexer.java
@@ -0,0 +1,270 @@
+package de.suma;
+
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.client.solrj.request.AbstractUpdateRequest;
+import org.apache.solr.client.solrj.request.SolrPing;
+import org.apache.solr.client.solrj.request.UpdateRequest;
+import org.apache.solr.client.solrj.response.SolrPingResponse;
+import org.apache.solr.common.SolrInputDocument;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Dieses Programm demonstriert die grundlegenden Schritte um mittels Java Dokumente in einem Solr-Server
+ * zu speichern.
+ *
+ * @author Sascha Szott
+ * @version 1.0
+ *
+ */
+public class SolrIndexer {
+
+    // wenn Sie das Programm innerhalb der Ubuntu-VM ausführen, dann schreiben Sie statt der IP einfach localhost
+    // in meinem Fall nutze ich die Entwicklungsumgebung direkt auf dem Wirt und lasse nur den Solr-Server in der VM laufen
+    // in der VM habe ich die IP-Adresse mittels des Befehls ifconfig ermittelt
+    private static final String SOLR_SERVER_URL = "http://192.168.144.131:8983/solr/";
+
+    // diesen Core haben wir am Dienstag angelegt
+    private static final String SOLR_CORE_NAME = "solr-ueb2";
+
+    // Zugangsdaten für den Zugriff auf den Solr-Server
+    public static final String USERNAME = "sumatech";
+    public static final String PASSWORD = "suma!tech$17";
+
+    private SolrClient solrClient;
+
+    /**
+     * Verbindung zum Solr-Server herstellen
+     */
+    private void connect() {
+
+        solrClient = new HttpSolrClient.Builder(SOLR_SERVER_URL + SOLR_CORE_NAME).build();
+
+        // Verbindung zum Solr-Server prüfen
+        try {
+            SolrPing ping = new SolrPing();
+            ping.setBasicAuthCredentials(USERNAME, PASSWORD);
+            SolrPingResponse pingResponse = ping.process(solrClient);
+            if (pingResponse.getStatus() != 0) {
+                System.out.println("Es gab einen unerwarteten Fehler beim Ping auf den Solr-Server (Status-Code ist " + pingResponse.getStatus() + ")");
+            }
+            else {
+                System.out.println("Ping zum Solr-Server war erfolgreich und dauerte " + pingResponse.getQTime() + " ms");
+            }
+        } catch (SolrServerException e) {
+            // do something reasonable
+            System.err.println(e);
+        } catch (IOException e) {
+            // do something reasonable
+            System.err.println(e);
+        }
+    }
+
+    /**
+     * bestehende Verbindung zum Solr-Server schließen
+     */
+    private void close() {
+        if (solrClient != null) {
+            try {
+                solrClient.close();
+            } catch (IOException e) {
+                // do something reasonable
+                e.printStackTrace();
+            }
+            finally {
+                solrClient = null;
+            }
+        }
+    }
+
+    /**
+     * Erzeugt aus dem übergebenen Dokument ein SolrInputDocument, das indexiert werden kann.
+     *
+     * @param gutenbergDoc Datenhaltungsobjekt aus der Applikation
+     * @return das zu indexierende Dokument
+     */
+    private SolrInputDocument buildSolrDoc(GutenbergDoc gutenbergDoc) {
+        SolrInputDocument document = new SolrInputDocument();
+        document.addField("id", gutenbergDoc.getDocId());
+
+        return document;
+    }
+
+    /**
+     * Indexiert das übergebene Dokument und übernimmt die Änderungen in den Solr-Index
+     * durch das Ausführen eines Commits.
+     *
+     * @param gutenbergDoc das zu indexierende Dokument
+     */
+    public void indexAndCommitOneDocument(GutenbergDoc gutenbergDoc) {
+        SolrInputDocument document = buildSolrDoc(gutenbergDoc);
+
+        UpdateRequest updateRequest = new UpdateRequest();
+        updateRequest.setBasicAuthCredentials(USERNAME, PASSWORD);
+        updateRequest.add(document);
+        updateRequest.setAction(AbstractUpdateRequest.ACTION.COMMIT, false, false);
+
+        try {
+            updateRequest.process(solrClient);
+            System.out.println("Dokument " + gutenbergDoc.getDocId() + " erfolgreich indexiert!");
+        }
+        catch (SolrServerException|IOException e) {
+            System.err.println("Fehler bei der Indexierung des Dokuments " + gutenbergDoc.getDocId() + ": " + e.getMessage());
+            updateRequest.rollback();
+        }
+    }
+
+    /**
+     * Indexiert die übergebene Liste von Dokumenten und führt am Ende ein Commit durch.
+     *
+     * Tritt während der Indexierung ein Fehler auf, so werden die Änderungen nicht
+     * zurückgenommen, die bereits erfolgreich in den Solr-Index geschrieben werden konnten.
+     *
+     * @param gutenbergDocs die Liste der zu indexierenden Dokumente
+     */
+    public void indexDocumentsAndCommit(List<GutenbergDoc> gutenbergDocs) {
+        UpdateRequest updateRequest = new UpdateRequest();
+        updateRequest.setBasicAuthCredentials(USERNAME, PASSWORD);
+        for (GutenbergDoc gutenbergDoc : gutenbergDocs) {
+            SolrInputDocument solrInputDocument = buildSolrDoc(gutenbergDoc);
+            if (gutenbergDoc.getDocId().equals("9")) {
+                // füge zu Dokument 9 ein Feld hinzu, dass es nicht im Solr-Schema gibt
+                solrInputDocument.addField("foo", "bar");
+            }
+            updateRequest.add(solrInputDocument);
+        }
+        updateRequest.setAction(AbstractUpdateRequest.ACTION.COMMIT, false, false);
+
+        try {
+            updateRequest.process(solrClient);
+            System.out.println(gutenbergDocs.size() + " Dokumente erfolgreich indexiert!");
+        }
+        catch (SolrServerException|IOException e) {
+            System.err.println("Fehler bei der Indexierung der Dokumente: " + e.getMessage());
+            updateRequest.rollback();
+        }
+    }
+
+    /**
+     * Indexiert die übergebene Liste von Dokumenten und führt nach jedem Dokument einen Commit durch.
+     *
+     * Tritt während der Indexierung eines Dokuments ein Fehler auf, so gehen die zuvor bereits
+     * erfolgreich indexierten Dokumente nicht verloren.
+     *
+     * @param gutenbergDocs die Liste der zu indexierenden Dokumente
+     */
+    public void indexAndCommitDocuments(List<GutenbergDoc> gutenbergDocs) {
+        for (GutenbergDoc gutenbergDoc : gutenbergDocs) {
+            UpdateRequest updateRequest = new UpdateRequest();
+            updateRequest.setBasicAuthCredentials(USERNAME, PASSWORD);
+
+            SolrInputDocument solrInputDocument = buildSolrDoc(gutenbergDoc);
+            if (gutenbergDoc.getDocId().equals("9")) {
+                // füge zu Dokument 9 ein Feld hinzu, dass es nicht im Solr-Schema gibt
+                solrInputDocument.addField("foo", "bar");
+            }
+            updateRequest.add(solrInputDocument);
+            updateRequest.setAction(AbstractUpdateRequest.ACTION.COMMIT, false, false);
+
+            try {
+                updateRequest.process(solrClient);
+                System.out.println("Dokument " + gutenbergDoc.getDocId() + " erfolgreich indexiert!");
+            }
+            catch (SolrServerException|IOException e) {
+                System.err.println("Fehler bei der Indexierung der Dokumente: " + e.getMessage());
+                updateRequest.rollback();
+            }
+        }
+    }
+
+    /**
+     * Stößt die Optimierung des Solr-Index an.
+     */
+    public void optimizeIndex() {
+        UpdateRequest updateRequest = new UpdateRequest();
+        updateRequest.setBasicAuthCredentials(USERNAME, PASSWORD);
+        updateRequest.setAction(AbstractUpdateRequest.ACTION.OPTIMIZE, false, false);
+        try {
+            updateRequest.process(solrClient);
+            System.out.println("Index erfolgreich optimiert!");
+        } catch (SolrServerException|IOException e) {
+            System.err.println("Fehler beim Index-Optimize: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Löscht das Dokument mit der übergebene ID aus dem Solr-Index.
+     *
+     * @param id die ID des zu löschenden Dokuments
+     */
+    public void deleteDocById(String id) {
+        UpdateRequest updateRequest = new UpdateRequest();
+        updateRequest.setBasicAuthCredentials(USERNAME, PASSWORD);
+        updateRequest.deleteById(id);
+        updateRequest.setAction(AbstractUpdateRequest.ACTION.COMMIT, false, false);
+        try {
+            updateRequest.process(solrClient);
+            System.out.println("Dokument mit ID " + id + " erfolgreich gelöscht!");
+        } catch (SolrServerException|IOException e) {
+            System.err.println("Fehler beim Löschen des Dokuments " + id + ": " + e.getMessage());
+        }
+    }
+
+    /**
+     * Entfernt alle Dokumente aus dem Solr-Index.
+     */
+    public void deleteAllDocs() {
+        UpdateRequest updateRequest = new UpdateRequest();
+        updateRequest.setBasicAuthCredentials(USERNAME, PASSWORD);
+        updateRequest.deleteByQuery("*:*");
+        updateRequest.setAction(AbstractUpdateRequest.ACTION.COMMIT, false, false);
+        try {
+            updateRequest.process(solrClient);
+            System.out.println("Alle Dokumente erfolgreich gelöscht!");
+        } catch (SolrServerException|IOException e) {
+            System.err.println("Fehler beim Löschen aller Dokumente: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+
+        SolrIndexer solrIndexer = new SolrIndexer();
+        solrIndexer.connect();
+
+        List<GutenbergDoc> gutenbergDocs = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            // prepare document and add to Solr index
+            GutenbergDoc doc = new GutenbergDoc();
+            doc.setDocId("" + i);
+            doc.setTitle("Tiny test document");
+            String[] subjectHeadings = new String[] {"Drama", "Computer networks", "Libraries"};
+            doc.setSubjectHeadings(Arrays.asList(subjectHeadings));
+            gutenbergDocs.add(doc);
+        }
+
+
+        if (false) {
+            for (GutenbergDoc gutenbergDoc : gutenbergDocs) {
+                solrIndexer.indexAndCommitOneDocument(gutenbergDoc);
+            }
+        }
+
+        //solrIndexer.indexDocumentsAndCommit(gutenbergDocs);
+
+        //solrIndexer.indexAndCommitDocuments(gutenbergDocs);
+
+        //solrIndexer.deleteDocById("6");
+
+        //solrIndexer.optimizeIndex();
+
+        //solrIndexer.deleteAllDocs();
+
+        solrIndexer.close();
+    }
+
+}
---
+++ ueb1-2018/src/main/java/de/suma/SolrSearcher.java
@@ -0,0 +1,190 @@
+package de.suma;
+
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.client.solrj.request.QueryRequest;
+import org.apache.solr.client.solrj.request.SolrPing;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.client.solrj.response.SolrPingResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class SolrSearcher {
+
+    private SolrClient solrClient;
+
+    /**
+     * Verbindung zum Solr-Server herstellen
+     */
+    private void connect() {
+
+        solrClient = new HttpSolrClient.Builder(Configuration.SOLR_SERVER_URL + Configuration.SOLR_CORE_NAME).build();
+
+        // Verbindung zum Solr-Server prüfen
+        try {
+            SolrPing ping = new SolrPing();
+            ping.setBasicAuthCredentials(Configuration.USERNAME, Configuration.PASSWORD);
+            SolrPingResponse pingResponse = ping.process(solrClient);
+            if (pingResponse.getStatus() != 0) {
+                System.out.println("Es gab einen unerwarteten Fehler beim Ping auf den Solr-Server (Status-Code ist " + pingResponse.getStatus() + ")");
+            }
+            else {
+                System.out.println("Ping zum Solr-Server war erfolgreich und dauerte " + pingResponse.getQTime() + " ms");
+            }
+        } catch (SolrServerException|IOException e) {
+            // do something reasonable
+            System.err.println(e);
+        }
+    }
+
+    /**
+     * bestehende Verbindung zum Solr-Server schließen
+     */
+    private void close() {
+        if (solrClient != null) {
+            try {
+                solrClient.close();
+            } catch (IOException e) {
+                // do something reasonable
+                e.printStackTrace();
+            }
+            finally {
+                solrClient = null;
+            }
+        }
+    }
+
+    /**
+     * Verwendung des Standard Query Parsers, der nur ein Index durchsucht (standardmäßig soll im Feld fulltext_stemmed
+     * gesucht werden)
+     *
+     * @param queryString Suchanfrage
+     */
+    private void runQuery(String queryString) {
+        SolrQuery query = new SolrQuery();
+
+        query.setQuery(queryString);
+        // TODO Standardsuchfeld definieren mittels Query-Parameter df
+        query.set("fl", "*,score"); // Feldliste im Resultat (* bedeutet, dass alle Felder zurückgegeben werden, die als stored="true" markiert sind)
+        query.setRows(10);
+
+        QueryRequest req = new QueryRequest(query);
+        req.setBasicAuthCredentials(Configuration.USERNAME, Configuration.PASSWORD);
+
+        QueryResponse response = null;
+        try {
+            response = req.process(solrClient);
+        } catch (SolrServerException|IOException e) {
+            // do something reasonable
+            System.err.println(e);
+        }
+
+        printRanking(response);
+
+    }
+
+    /**
+     * Verwendung des DisMax Query Parsers, um eine feldübergreifende Suche zu ermöglichen
+     *
+     * Unterschiedliche Gewichtung der Suchfelder gemäß Aufgabenstellung
+     *
+     * Boosting auf Basis der Anzahl der Downloads in den letzten 30 Tagen (Log-gewichtet)
+     *
+     * @param queryString Suchanfrage
+     */
+    private void runQueryOnMultipleFields(String queryString) {
+        SolrQuery query = new SolrQuery();
+
+        query.setQuery(queryString);
+
+        // TODO DisMax Query Parser verwenden und geeignete Parameter setzen
+        query.set("defType", "TODO");
+        query.set("qf", "TODO");
+        query.set("bf", "TODO");
+        query.set("fl", "*,score"); // Feldliste im Resultat (* bedeutet, dass alle Felder zurückgegeben werden, die als stored="true" markiert sind)
+        query.setRows(10);
+
+        QueryRequest req = new QueryRequest(query);
+        req.setBasicAuthCredentials(Configuration.USERNAME, Configuration.PASSWORD);
+
+        QueryResponse response = null;
+        try {
+            response = req.process(solrClient);
+        } catch (SolrServerException|IOException e) {
+            // do something reasonable
+            System.err.println(e);
+        }
+
+        printRanking(response);
+
+    }
+
+    /**
+     * Ausgabe des Suchtreffer im Ranking
+     * @param response
+     */
+    private void printRanking(QueryResponse response) {
+        if (response != null) {
+            SolrDocumentList results = response.getResults();
+
+            if (results.isEmpty()) {
+                System.out.println("Ihre Suchanfrage ergab leider keine Treffer. Bitte modifizieren Sie ihre Anfrage.");
+            }
+            else {
+                System.out.println("Trefferanzahl: " + results.getNumFound());
+                System.out.println("~~~~~~~~~ Trefferliste ~~~~~~~");
+                int rank = 1;
+                for (SolrDocument doc : results) {
+                    System.out.println("Treffer #" + rank);
+                    System.out.println("\tID: " + doc.get("id"));
+                    System.out.println("\tTitel: " + doc.get("title"));
+
+                    // TODO weitere Indexfelder (mit stored=true) ausgeben
+
+                    System.out.println("\tScore: " + doc.get("score"));
+                    System.out.println("\n- - - - - - - - - - -");
+                    rank++;
+                }
+            }
+        }
+    }
+
+    /**
+     * Einstiegsmethode
+     *
+     * Liest solange Suchanfragen vom Benutzer ein, bis dieser "quit!" eingibt.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        SolrSearcher solrSearcher = new SolrSearcher();
+        solrSearcher.connect();
+
+        Scanner input = new Scanner(System.in);
+        String query;
+
+        while (true) {
+            System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            System.out.print("search query >>> ");
+            query = input.nextLine();
+
+            if (query.equals("quit!")) {
+                break;
+            }
+
+            solrSearcher.runQuery(query);
+            // TODO solrSearcher.runQueryOnMultipleFields(query);
+        }
+
+        input.close();
+        solrSearcher.close();
+        System.out.println("Good Bye!");
+    }
+
+}
