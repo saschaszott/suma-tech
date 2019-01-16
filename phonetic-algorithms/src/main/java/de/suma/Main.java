7b8c4f18955117411b353e2cd108e57d2f7d5d98
Sascha Szott <szott@gmx.de>
2019-01-17 00:02:27 +0100

    make computation more robust: do not try to compute Soundex encodings for non-ASCII strings

--- phonetic-algorithms/src/main/java/de/suma/Main.java
+++ phonetic-algorithms/src/main/java/de/suma/Main.java
@@ -27,23 +27,28 @@ public class Main {
 
             System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
-            // Soundex Code ist immer vierstellig
+            // Soundex Code ist immer vierstellig (erste Stelle ist ein Buchstaben; dann folgen drei Ziffern aus {1,...6}
             Soundex soundex = new Soundex();
 
-            String soundexEncodedS1 = soundex.encode(s1);
-            System.out.println("soundex encoding of " + s1 + " is " + soundexEncodedS1);
-
-            String soundexEncodedS2 = soundex.encode(s2);
-            System.out.println("soundex encoding of " + s2 + " is " + soundexEncodedS2);
-
             try {
+                String soundexEncodedS1 = soundex.encode(s1);
+                System.out.println("soundex encoding of " + s1 + " is " + soundexEncodedS1);
+
+                String soundexEncodedS2 = soundex.encode(s2);
+                System.out.println("soundex encoding of " + s2 + " is " + soundexEncodedS2);
+
+
                 // returns the number of characters in the two encoded Strings that are the same.
                 // This return value ranges from 0 through 4: 0 indicates little or no similarity, and 4 indicates
                 // strong similarity or identical values.
                 int diff = soundex.difference(s1, s2);
                 System.out.println("soundex_diff(" + s1 + ", " + s2 + ") = " + diff);
-            } catch (EncoderException e) {
+            }
+            catch (EncoderException e) {
                 System.err.println("unerwarteter Fehler bei der Codierung: " + e.getMessage());
+            }
+            catch (IllegalArgumentException e) {
+                System.err.println("Soundex kann nur für ASCII-Zeichen berechnet werden");
             }
 
             System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
@@ -51,18 +56,22 @@ public class Main {
             // refined soundex code is optimized for spell checking words.
             RefinedSoundex refinedSoundex = new RefinedSoundex();
 
-            String refinedSoundexEncodedS1 = refinedSoundex.encode(s1);
-            System.out.println("refined soundex encoding of " + s1 + " is " + refinedSoundexEncodedS1);
-
-            String refinedSoundexEncodedS2 = refinedSoundex.encode(s2);
-            System.out.println("refined soundex encoding of " + s2 + " is " + refinedSoundexEncodedS2);
-
             try {
+                String refinedSoundexEncodedS1 = refinedSoundex.encode(s1);
+                System.out.println("refined soundex encoding of " + s1 + " is " + refinedSoundexEncodedS1);
+
+                String refinedSoundexEncodedS2 = refinedSoundex.encode(s2);
+                System.out.println("refined soundex encoding of " + s2 + " is " + refinedSoundexEncodedS2);
+
                 // for refined Soundex, the return value can be greater than 4.
                 int diff = refinedSoundex.difference(s1, s2);
                 System.out.println("refinedSoundex_diff(" + s1 + ", " + s2 + ") = " + diff);
-            } catch (EncoderException e) {
+            }
+            catch (EncoderException e) {
                 System.err.println("unerwarteter Fehler bei der Codierung: " + e.getMessage());
+            }
+            catch (ArrayIndexOutOfBoundsException e) {
+                System.err.println("Refined Soundex kann nur für ASCII-Zeichen berechnet werden");
             }
 
             System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
@@ -77,7 +86,7 @@ public class Main {
             System.out.println("cologne phonetic encoding of " + s2 + " is " + colognePhoneticEncodedS2);
 
             boolean phoneticEqual = colognePhonetic.isEncodeEqual(s1, s2);
-            System.out.println(s1 + " and " + s2 + " are phonetic equal is " + phoneticEqual);
+            System.out.println(s1 + " and " + s2 + " are equal with respect to cologne phonetic: " + phoneticEqual);
 
             System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
