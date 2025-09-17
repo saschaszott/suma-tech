2030f7e52fa9e1911a7014668c7c17ec9995bf75
Sascha Szott <szott@gmx.de>
2025-09-17 17:36:36 +0200

    Add current date print statement to script

--- praxisteile/XX-git-update-workflow/current-datetime.py
+++ praxisteile/XX-git-update-workflow/current-datetime.py
@@ -3,6 +3,7 @@ from datetime import datetime
 def main():
     now = datetime.now()
     print("Aktuelle Systemzeit:", now.strftime("%H:%M:%S"))
+    print("Aktuelles Datum:", now.strftime("%Y-%m-%d")) # <-- neue Zeile hinzugefügt
 
 if __name__ == "__main__":
     main()
