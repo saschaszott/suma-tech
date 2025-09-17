614b5b3757985aab1ce83c223938808fbcaef85a
Sascha Szott <szott@gmx.de>
2025-09-17 17:42:38 +0200

    Update current-datetime.py with new print statement
    Added a comment and a new print statement for the current date.

--- praxisteile/XX-git-update-workflow/current-datetime.py
+++ praxisteile/XX-git-update-workflow/current-datetime.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 def main():
     now = datetime.now()
+    # ein Kommentar, der vom Dozenten eingefügt wurde <-- weitere Änderung
     print("Aktuelle Systemzeit:", now.strftime("%H:%M:%S"))
     print("Aktuelles Datum:", now.strftime("%Y-%m-%d")) # <-- neue Zeile hinzugefügt
 
