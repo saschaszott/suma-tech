a6bed45548a51515a8eb0a7e931b2438c84587df
Sascha Szott <szott@gmx.de>
2025-09-20 18:21:16 +0200

    Formatiere den Code für die TF-IDF-Gewichtung zur Verbesserung der Lesbarkeit und korrigiere einen Fehler in der Berechnung der logarithmischen Werte.

--- praxisteile/06/03_tf-idf-weighting.py
+++ praxisteile/06/03_tf-idf-weighting.py
@@ -2,7 +2,12 @@ import pandas as pd
 import numpy as np
 
 # Term Frequencies
-tf = {'Affe': [2, 1, 0], 'Bär': [1, 0, 2], 'Maus': [0, 0, 1], 'Zebra': [0, 1, 0]}
+tf = {
+    'Affe':  [2, 1, 0],
+    'Bär':   [1, 0, 2],
+    'Maus':  [0, 0, 1],
+    'Zebra': [0, 1, 0]
+}
 df = pd.DataFrame(tf, index=['D1', 'D2', 'D3'])
 
 print("== Term Frequencies ==\n", df)
@@ -14,8 +19,8 @@ N = len(df)
 df_counts = (df > 0).sum()
 
 # Inverse Document Frequencies (logarithmisch)
-idf_log = np.log(N / df_counts)
+idf_log = np.log(N / df_counts) # natürlicher Logarithmus (zur Basis e): ln
 
-tf_log = df.replace(0, np.nan).map(lambda x: np.log(x) + 1).fillna(0)
+tf_log = df.replace(0, np.nan).map(lambda x: np.log(x + 1)).fillna(0) # ln
 
 print("\n== TF-IDF-Termgewichtsmatrix ==\n", tf_log.multiply(idf_log).round(3))
