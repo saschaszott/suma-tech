e69ff3198f474b8df0137217728759dfee215301
Sascha Szott <szott@gmx.de>
2026-05-08 15:41:06 +0200

    Aktualisiere download_file-Funktion: Entferne spezifische UTF-8-Beschreibung und passe URL für den Download an

--- praxisteile/01/01_download.py
+++ praxisteile/01/01_download.py
@@ -2,8 +2,7 @@ import requests
 
 def download_file(url, filename):
     """
-    Lädt eine UTF-8-codierte Datei von der angegebenen URL herunter 
-    und speichert sie als UTF-8-codierte Textdatei unter dem angegebenen Namen.
+    Lädt eine Datei von der angegebenen URL herunter und speichert sie lokal unter dem angegebenen Namen.
 
     Parameter:
     - url (str): URL der Datei, die heruntergeladen werden soll.
@@ -21,6 +20,6 @@ def download_file(url, filename):
         print(f"Fehler beim Herunterladen der Datei: {response.status_code}")
 
 if __name__ == "__main__":
-    url = "https://www.gutenberg.org/files/21000/21000-0.txt"
+    url = "https://www.gutenberg.org/cache/epub/21000/pg21000.txt"
     filename = "21000.txt"
     download_file(url, filename)
