0a466b3b204c275b72507658ec911f1e7edcafa7
Sascha Szott <szott@gmx.de>
2026-09-09 13:45:57 +0200

    Änderungen für Demo zurückgesetzt

--- praxisteile/XX-git-update-workflow/current-datetime.py
+++ praxisteile/XX-git-update-workflow/current-datetime.py
@@ -1,11 +1,8 @@
-# ein Kommentar vom Dozenten
 from datetime import datetime
 
 def main():
     now = datetime.now()
-    # ein Kommentar, der vom Dozenten eingefügt wurde <-- weitere Änderung
     print("Aktuelle Systemzeit:", now.strftime("%H:%M:%S"))
-    print("Aktuelles Datum:", now.strftime("%Y-%m-%d")) # <-- neue Zeile hinzugefügt
 
 if __name__ == "__main__":
     main()
