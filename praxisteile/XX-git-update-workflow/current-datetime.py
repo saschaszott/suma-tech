81655808bc7bbdc08caee25fc00c9bdf7c9ce8ec
Sascha Szott <szott@gmx.de>
2026-09-10 14:29:59 +0200

    Ausgabe aktuelles Datum

--- praxisteile/XX-git-update-workflow/current-datetime.py
+++ praxisteile/XX-git-update-workflow/current-datetime.py
@@ -3,6 +3,7 @@ from datetime import datetime
 def main():
     now = datetime.now()
     print("Aktuelle Systemzeit:", now.strftime("%H:%M:%S"))
+    print("Aktuelles Datum:", now.strftime("%Y-%m-%d"))
 
 if __name__ == "__main__":
     main()
